b86c8e21dd5b3083565b4fa1b355ec7016f7d950
Joe Peck <joe@Joes-MacBook-Pro.local>
2017-12-07 22:16:01 -0500

    Parts one n' two in Elixir, shabaaaam

--- day_04/passphrase_validator.exs
+++ day_04/passphrase_validator.exs
@@ -1,13 +1,32 @@
 defmodule PassphraseValidator do
   def is_valid(passphrase) do
-    original_list = String.split(passphrase)
-    length(original_list) == (Enum.uniq(original_list) |> length)
+    passphrase
+    |> String.split
+    |> aint_no_dupes
   end
 
   def count_valid_passphrases_in_file do
     line_by_line('input.txt')
     |> Enum.filter(fn(line) -> is_valid(line) end)
     |> length
+  end
+
+  def is_valid_part_two(passphrase) do
+    passphrase
+    |> String.split
+    |> Enum.map(fn(word) -> alphabetized_word(word) end) # I know there's a shorthand way to do this...
+    |> aint_no_dupes
+  end
+
+  defp aint_no_dupes(list) do
+    length(list) == (Enum.uniq(list) |> length)
+  end
+
+  defp alphabetized_word(word) do
+    word
+    |> String.graphemes
+    |> Enum.sort
+    |> Enum.join(" ")
   end
 
   defp line_by_line(path) do
--- day_04/passphrase_validator_test.exs
+++ day_04/passphrase_validator_test.exs
@@ -17,4 +17,25 @@ defmodule PassphraseValidatorTest do
   test "aa bb cc dd aaa" do
     assert PassphraseValidator.is_valid("aa bb cc dd aaa") == true
   end
+
+  # Part two
+  test "abcde fghij" do
+    assert PassphraseValidator.is_valid_part_two("abcde fghij") == true
+  end
+
+  test "abcde xyz ecdab" do
+    assert PassphraseValidator.is_valid_part_two("abcde xyz ecdab") == false
+  end
+
+  test "a ab abc abd abf abj" do
+    assert PassphraseValidator.is_valid_part_two("a ab abc abd abf abj") == true
+  end
+
+  test "iiii oiii ooii oooi oooo" do
+    assert PassphraseValidator.is_valid_part_two("iiii oiii ooii oooi oooo") == true
+  end
+
+  test "oiii ioii iioi iiio" do
+    assert PassphraseValidator.is_valid_part_two("oiii ioii iioi iiio") == false
+  end
 end
